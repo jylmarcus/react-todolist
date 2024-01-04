32a1d2c0fc15d5b90936db26f3ce769514694ceb
Hamukaze <maplecow@gmail.com>
2024-01-04 21:46:11 +0800

    Initial todo styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Todo from './components/todo/todo';
 
+const todoList = [
+                {title: "todo1", description: "desc1", date: "date1"},
+                {title: "todo2", description: "desc1", date: "date1"},
+                {title: "todo3", description: "desc1", date: "date1"}
+              ]
+
 function App() {
+  const todos = todoList.map(todo => 
+      <Todo key={todo.title} title={todo.title} description={todo.description} date={todo.date} />
+    )
   return (
-    <div className="App">
-      <Todo title="test" description="testing" date="testDate" />
+    <div className="flex box-border font-roboto flex-col h-screen p-5 items-center">
+      {todos}
     </div>
   );
 }
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,7 +1,7 @@
 export default function Todo({ title, description, date}) {
     return (
-        <div>
-            <p>{ title }</p>
+        <div className="border-indigo-400 border-2 border-solid rounded min-w-[60%] my-2 p-2">
+            <p className="text-xl capitalize font-bold">{ title }</p>
             <p>{ description }</p>
             <p>{ date }</p>
         </div>
